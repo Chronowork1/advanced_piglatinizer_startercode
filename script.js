f8852185cb1f14a2c590ed0221841e3efbc18702
Jian Xie <jixie1@s.sfusd.edu>
2019-01-23 06:58:37 +0000

    working

--- script.js
+++ script.js
@@ -7,18 +7,49 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 
 $(document).ready(function(){
-  function Vowels(){
-      let word = $("input").val();
-      let otherWords = word.substring(1);
-      if(word.charAt(0) === "a"|| word.charAt(0) === "e"|| word.charAt(0) ==="i" || word.charAt(0) ==="o" || word.charAt(0) === "u"){
-        return word = word + "yay";
-      } else {
-        return otherWords + word.charAt(0) + "ay";
-      }
+  
+  /*function consonants(){
+    let consonant= ["b","c",'d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+    let consonantLength = consonant.length;
+    for(let i = 0; i< consonantLength; i++){
+      consonant[i];
+    }
+    return consonantLength;
   }
+  console.log(consonants());
+  */
+  function Vowels(){
+    let word = $("input").val();
+    let otherWords = word.substring(1);
+    if(word.charAt(0) === "a"|| word.charAt(0) === "e"|| word.charAt(0) ==="i" || word.charAt(0) ==="o" || word.charAt(0) === "u"){
+      return word = word + "yay";
+    } else {
+      return otherWords + word.charAt(0) + "ay";
+    }
+  }
+  
+  function Sentences(){
+    let sentence = $("input").val();
+    let splitSentence = sentence.split(" ");
+    let otherWords = splitSentence.length.substring(1);
+    for (var i = 0; i < splitSentence.length; i++) {
+      if(splitSentence.length.charAt(0) === "a"|| splitSentence.length.charAt(0) === "e"|| splitSentence.length.charAt(0) ==="i" || splitSentence.length.charAt(0) ==="o" || splitSentence.length.charAt(0) === "u"){
+        return splitSentence.length = splitSentence.length + "yay";
+      } else {
+        return otherWords + splitSentence.length.charAt(0) + "ay";
+      }
+    }
+    return Vowels[i];
+  }
+
+  //var myStringArray = ["Hello","World"];
+    //var arrayLength = myStringArray.length;
+          //console.log(splitSentence[i]);
+    //Do something
+  
   $(".translate").click(function(){
     console.log(Vowels());
-    Vowels();
+    $(".output").text(Vowels());
   });
 });
   
